49e06ed24e3fc1406f50513b41254c8aba1b88ee
Ahmed Bedair <abedair@gmail.com>
2023-04-10 19:18:19 -0400

    committing changes to pom.xml and HellocdApplication.java

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -1,13 +1,31 @@
 package com.example.hellocd;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HellocdApplication {
 
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HellocdApplication.class, args);
 	}
 
 }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
